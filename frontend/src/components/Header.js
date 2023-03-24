1ab915c0ede6347d2fe1aa4438e7fee2fb43fb13
OscarSantos1 <ossantos1994@gmail.com>
2023-03-23 21:26:17 -0600

    header & aboutme font size based on width

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,10 +3,10 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1 className="text-2xl sm:text-4xl md:text-6xl 2xl:text-[113px]">
+      <h1 className="text-2xl sm:text-4xl md:text-6xl 2xl:text-[4vw]">
         Oscar de los Santos
       </h1>
-      <p className="text-sm sm:text-lg ml-2 2xl:ml-4 2xl:text-5xl">
+      <p className="text-sm sm:text-lg ml-2 2xl:ml-[0.5%] 2xl:text-[1.2vw]">
         Full-Stack Developer
       </p>
     </div>
