2740c790c0471c018ca9db93082b9b63239015fd
OscarSantos1 <ossantos1994@gmail.com>
2023-03-26 23:28:37 -0600

    descriptions updated

--- frontend/src/components/Icon.js
+++ frontend/src/components/Icon.js
@@ -3,7 +3,7 @@ import { IoIosCheckmarkCircle, IoIosCart, IoMdBulb } from "react-icons/io";
 const Icon = ({ title }) => {
   return (
     <>
-      {title == "Do me." ? (
+      {title == "Diurnal" ? (
         <IoIosCheckmarkCircle className="text-white h-[80%] w-[50%]" />
       ) : title == "notARealStore" ? (
         <IoIosCart className="text-white h-[80%] w-[50%]" />
